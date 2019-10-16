682e26dc37ac14b285291793d8f9e9e440fe3522
johny <jho406@gmail.com>
2019-10-16 16:52:36 -0400

    refactor transform methods to use a transform_values helper

--- lib/transformer.rb
+++ lib/transformer.rb
@@ -3,49 +3,46 @@ require 'date'
 module Transfomer
   extend self
 
-  def transform_comma(records = [])
+  def transform_values(records = [])
     return [] if records.empty?
 
     records.map do |record|
       new_record = {}
       record.each_pair do |k, v|
-        if k == :sex
-          v = v.downcase.to_sym
-        end
-
-        if k == :dob
-          v = Date.strptime(v, '%m/%d/%Y')
-        end
-
-        new_record[k] = v
+        new_record[k] = yield k, v
       end
 
       new_record
     end
   end
 
-  def transform_pipe(records = [])
-    return [] if records.empty?
-
-    records.map do |record|
-      new_record = {}
-      record.each_pair do |k, v|
-        if k == :sex
-          v = if v.downcase == 'm'
-            :male
-          else
-            :female
-          end
-        end
-
-        if k == :dob
-          v = Date.strptime(v, '%m-%d-%Y')
-        end
-
-        new_record[k] = v
+  def transform_comma(records = [])
+    transform_values(records) do |k, v|
+      case k
+        when :sex
+          v.downcase.to_sym
+        when :dob
+          Date.strptime(v, '%m/%d/%Y')
+        else
+          v
       end
+    end
+  end
 
-      new_record
+  def transform_pipe(records = [])
+    transform_values(records) do |k, v|
+      case k
+      when :sex
+        if v.downcase == 'm'
+          :male
+        else
+          :female
+        end
+      when :dob
+        Date.strptime(v, '%m-%d-%Y')
+      else
+        v
+      end
     end
   end
 end
