f87d5a6db84c694fd71486120441f1e57cf269f0
johny <jho406@gmail.com>
2019-10-15 23:16:52 -0400

    Add test and implementation for empty

---
+++ lib/parser.rb
@@ -0,0 +1,7 @@
+module Parser
+  extend self
+
+  def parse_csv(str)
+    return [] if str.empty?
+  end
+end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,9 +1,11 @@
 require 'rspec'
+require_relative '../lib/parser'
 
 RSpec.describe 'Parser' do
   context '.parse_csv' do
-    it 'returns empty if called with empty obj' do
-      results = Parser.parse_csv()
+    it 'returns empty if called with empty str' do
+      csv_str = ''
+      results = Parser.parse_csv(csv_str)
       expect(results).to eql []
     end
   end
