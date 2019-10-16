907290f7568e2e123f0ca4edde566dad40c16fd1
johny <jho406@gmail.com>
2019-10-16 16:40:54 -0400

    add pipe transfomer

--- lib/transformer.rb
+++ lib/transformer.rb
@@ -23,4 +23,25 @@ module Transfomer
       new_record
     end
   end
+
+  def transform_pipe(records = [])
+    return [] if records.empty?
+
+    records.map do |record|
+      new_record = {}
+      record.each_pair do |k, v|
+        if k == :sex
+          v = v.downcase.to_sym
+        end
+
+        if k == :dob
+          v = Date.strptime(v, '%m-%d-%Y')
+        end
+
+        new_record[k] = v
+      end
+
+      new_record
+    end
+  end
 end
--- spec/transformer_spec.rb
+++ spec/transformer_spec.rb
@@ -65,5 +65,68 @@ RSpec.describe 'Transformer' do
       ])
     end
   end
+
+  context '.transform_pipe' do
+    it 'returns a blank hash when passed an empty hash' do
+      records = []
+      results = Transfomer.transform_pipe(records)
+      expect(results).to eql([])
+    end
+
+    it 'returns first_name and last_name untouched' do
+      records = [{
+        first_name: 'Kelly',
+        last_name: 'Sue',
+      }]
+
+      results = Transfomer.transform_pipe(records)
+      expect(results).to eql([{
+        first_name: 'Kelly',
+        last_name: 'Sue'
+      }])
+    end
+
+    it 'transforms the sex to a lowercase symbol of a single rows' do
+      records = [{
+        sex: 'Male'
+      }]
+
+      results = Transfomer.transform_pipe(records)
+      expect(results).to eql([{
+        sex: :male
+      }])
+    end
+
+    it 'transforms the sex to a lowercase symbol of multiple rows' do
+      records = [{
+        sex: 'Male'
+      },
+      {
+        sex: 'Female'
+      }]
+
+      results = Transfomer.transform_pipe(records)
+      expect(results[0]).to eql({
+        sex: :male
+      })
+
+      expect(results[1]).to eql({
+        sex: :female
+      })
+    end
+
+    it 'transforms the dob to a date' do
+      records = [
+        {dob: '2-13-1943'},
+        {dob: '4-23-1967'}
+      ]
+
+      results = Transfomer.transform_pipe(records)
+      expect(results).to eql([
+        {dob: Date.new(1943, 2, 13)},
+        {dob: Date.new(1967, 4, 23)}
+      ])
+    end
+  end
 end
 
