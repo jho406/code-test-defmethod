8110c0b05f8ef146c85d37e64dd1ab1611e195fd
johny <jho406@gmail.com>
2019-10-16 21:59:29 -0400

    Add test and implementation for custom title and actual records

--- lib/code_test/output.rb
+++ lib/code_test/output.rb
@@ -3,9 +3,25 @@ module CodeTest
     extend self
 
     def view(records=[], title='My Records:')
+      body = records.map do |line|
+        last_name = line[:last_name]
+        first_name = line[:first_name]
+        sex = line[:sex]
+        dob = line[:dob].strftime('%-m/%-d/%Y')
+        color = line[:color]
+
+        "#{last_name} #{first_name} #{sex} #{dob} #{color}"
+      end
+
+      body_str = if body.empty?
+                   'No records'
+                 else
+                   body.join("\n")
+                 end
+
       <<~OUTPUT
       #{title}
-      No records
+      #{body_str}
       OUTPUT
     end
   end
--- spec/output_spec.rb
+++ spec/output_spec.rb
@@ -12,6 +12,40 @@ RSpec.describe 'CodeTest::Output' do
       No records
       OUTPUT
     end
+
+    it 'shows a different title' do
+      records = []
+      output = CodeTest::Output.view(records, 'Foobar records:')
+      expect(output).to eql <<~OUTPUT
+      Foobar records:
+      No records
+      OUTPUT
+    end
+
+    it 'shows a single formatted record' do
+      records = [
+        {last_name: 'Smith', first_name: 'John', sex: 'Male', dob: Date.new(2014, 1, 20), color: 'Tan'}
+      ]
+      output = CodeTest::Output.view(records)
+      expect(output).to eql <<~OUTPUT
+      My Records:
+      Smith John Male 1/20/2014 Tan
+      OUTPUT
+    end
+
+    it 'shows a long list of formatted records and custom title' do
+      records = [
+        {last_name: 'Smith', first_name: 'John', sex: 'Male', dob: Date.new(2014, 1, 20), color: 'Tan'},
+        {last_name: 'Smith', first_name: 'John', sex: 'Male', dob: Date.new(2014, 1, 20), color: 'Blue'}
+      ]
+      output = CodeTest::Output.view(records, "Output 100:")
+      expect(output).to eql <<~OUTPUT
+      Output 100:
+      Smith John Male 1/20/2014 Tan
+      Smith John Male 1/20/2014 Blue
+      OUTPUT
+    end
   end
 end
+
 
