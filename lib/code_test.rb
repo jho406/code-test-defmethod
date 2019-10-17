5f78d19e77a5e5e1cb88a9f853609df7661c8345
johny <jho406@gmail.com>
2019-10-17 00:22:24 -0400

    refactor move show_by to output.rb

--- lib/code_test.rb
+++ lib/code_test.rb
@@ -13,38 +13,11 @@ module CodeTest
 
   def run
     records = import_input
-    output_1 = show_by_gender_sex_asc_then_last_asc(records)
-    output_2 = show_by_dob_asc_then_last_name_asc(records)
-    output_3 = show_by_last_name_dsc(records)
+    output_1 = Output.show_by_gender_sex_asc_then_last_asc(records)
+    output_2 = Output.show_by_dob_asc_then_last_name_asc(records)
+    output_3 = Output.show_by_last_name_dsc(records)
 
     puts [output_1, output_2, output_3].join("\n")
-  end
-
-  def show_by_gender_sex_asc_then_last_asc(records=[])
-    #Output 1 - sorted by gender (females before males) then by last name ascending
-    sorted_records = records.sort_by do |record|
-      [record[:sex], record[:last_name]]
-    end
-
-    Output.view(sorted_records, 'Output 1:')
-  end
-
-  def show_by_dob_asc_then_last_name_asc(records=[])
-    #Output 2 - sorted by birth date, ascending then by last name ascending
-    sorted_records = records.sort_by do |record|
-      [record[:dob], record[:last_name]]
-    end
-
-    Output.view(sorted_records, 'Output 2:')
-  end
-
-  def show_by_last_name_dsc(records=[])
-    #Output 3 - sorted by last name, descending
-    sorted_records = records.sort do |top, bottom|
-      bottom[:last_name] <=> top[:last_name]
-    end
-
-    Output.view(sorted_records, 'Output 3:')
   end
 
   def import_input
--- lib/code_test/output.rb
+++ lib/code_test/output.rb
@@ -24,5 +24,32 @@ module CodeTest
       #{body_str}
       OUTPUT
     end
+
+    def show_by_gender_sex_asc_then_last_asc(records=[])
+      #Output 1 - sorted by gender (females before males) then by last name ascending
+      sorted_records = records.sort_by do |record|
+        [record[:sex], record[:last_name]]
+      end
+
+      view(sorted_records, 'Output 1:')
+    end
+
+    def show_by_dob_asc_then_last_name_asc(records=[])
+      #Output 2 - sorted by birth date, ascending then by last name ascending
+      sorted_records = records.sort_by do |record|
+        [record[:dob], record[:last_name]]
+      end
+
+      view(sorted_records, 'Output 2:')
+    end
+
+    def show_by_last_name_dsc(records=[])
+      #Output 3 - sorted by last name, descending
+      sorted_records = records.sort do |top, bottom|
+        bottom[:last_name] <=> top[:last_name]
+      end
+
+      view(sorted_records, 'Output 3:')
+    end
   end
 end
