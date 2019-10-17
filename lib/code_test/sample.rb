4d1773c492a26653d5d38a22b2d4d4587d7521e5
johny <jho406@gmail.com>
2019-10-16 21:09:25 -0400

    Move sample import to sample module

--- lib/code_test/sample.rb
+++ lib/code_test/sample.rb
@@ -2,6 +2,26 @@ module CodeTest
   module Sample
     extend self
 
+    FILES_HEADERS_AND_DELIMITERS = [
+      ['comma.txt', [:last_name, :first_name, :sex, :color, :dob], ','],
+      ['pipe.txt',  [:last_name, :first_name, :initial, :sex, :color, :dob], '|'],
+      ['space.txt', [:last_name, :first_name, :initial, :sex, :dob, :color], ' '],
+    ]
+
+    def import
+      all_records = FILES_HEADERS_AND_DELIMITERS.flat_map do |v|
+        file_name, headers, delimiter = v
+
+        current_path = File.dirname(__FILE__)
+        comma_file = '../input_files/comma.txt'
+        full_path = File.join(current_path, '../input_files', file_name)
+
+        Parser.parse_from_file(full_path, headers: headers, delimiter: delimiter)
+      end
+
+      transform_records(all_records)
+    end
+
     def transform_records(records = [])
       Transfomer.transform_values(records) do |k, v|
         v = v.strip
--- spec/code_test_spec.rb
+++ spec/code_test_spec.rb
@@ -2,29 +2,5 @@ require 'rspec'
 require_relative '../lib/code_test'
 require 'byebug'
 
-RSpec.describe do
-  context 'import_sample_files' do
-    it 'imports comma.txt' do
-      comma_records = [
-        {last_name: 'Abercrombie', first_name: 'Neil', sex: :male, color: 'Tan', dob: Date.new(1943, 2, 13)},
-        {last_name: 'Bishop', first_name: 'Timothy', sex: :male, color: 'Yellow', dob: Date.new(1967, 4, 23)},
-        {last_name: 'Kelly', first_name: 'Sue', sex: :female, color: 'Pink', dob: Date.new(1959, 7, 12)}
-      ]
-
-      pipe_records = [
-        {last_name: 'Smith', first_name: 'Steve', initial: 'D', sex: :male, color: 'Red', dob: Date.new(1985, 3, 3)},
-        {last_name: 'Bonk', first_name: 'Radek', initial: 'S', sex: :male, color: 'Green', dob: Date.new(1975, 6, 3)},
-        {last_name: 'Bouillon', first_name: 'Francis', initial: 'G', sex: :male, color: 'Blue', dob: Date.new(1975, 6, 3)}
-      ]
-
-      space_records = [
-        {last_name: 'Kournikova', first_name: 'Anna', initial: 'F', sex: :female, color: 'Red', dob: Date.new(1975, 6, 3)},
-        {last_name: 'Hingis', first_name: 'Martina', initial: 'M', sex: :female, color: 'Green', dob: Date.new(1979, 4, 2)},
-        {last_name: 'Seles', first_name: 'Monica', initial: 'H', sex: :female, color: 'Black', dob: Date.new(1973, 12, 2)}
-      ]
-
-      result = CodeTest.import_sample_files
-      expect(result).to eql(comma_records + pipe_records + space_records)
-    end
-  end
+RSpec.describe 'CodeTest' do
 end
